10a940e83a8244abfacb887da1bd11c626ed65fc
Douglas Wu <dgrey1116@gmail.com>
2015-03-30 00:32:46 +0800

    finished the first version of error handle

--- prj01.cpp
+++ prj01.cpp
@@ -37,17 +37,22 @@
 #define J 0x02
 #define JAL 0x03
 
-#define MEM_SIZE 1000
+#define MEM_SIZE 1024
 
-unsigned int imem[MEM_SIZE];
-unsigned int dimg[MEM_SIZE];
-unsigned int dmem[MEM_SIZE];
+#define WRITE_ZERO 0
+#define NUMBER_OVERFLOW 1
+#define ADDRESS_OVERFLOW 2
+#define MISALIGNMENT 3
+
+unsigned int imem[MEM_SIZE/4];
+unsigned int dmem[MEM_SIZE/4];
 unsigned int sp;
 unsigned int iSize, dSize;
 unsigned int reg[32];
 unsigned int PC, cycle;
 unsigned int PC_init;
 bool pcChanged;
+bool error_halt;
 
 FILE *snapshot, *error_dump;
 
@@ -57,6 +62,9 @@ void print_cycle(void);
 void R_type(unsigned int);
 void I_type(unsigned int);
 void J_type(unsigned int);
+void print_error(int,int);
+bool has_overflow(int, int, int);
+void check_errors(int, int, int, int, int);
 int main()
 {
     if(!load_image()){
@@ -67,27 +75,31 @@ int main()
 
     cycle = 0;
     PC = PC_init;
+    error_halt = false;
     print_cycle();
     int i = 0;
     while( imem[i] != HALT ){
        // printf("0x%08x\n",imem[i]);
 
-        int opcode;
+        int opcode = imem[i] >> 26;;
         pcChanged = false;
-        opcode = imem[i] >> 26;
         printf("%08x  0x%02x ",imem[i], opcode);
-        if(opcode==0x00)
+        cycle++;//先增值因為error message會用到
+        if(opcode==0x00){
             R_type(imem[i]);
+        }
         else if(opcode == J || opcode == JAL){
             J_type(imem[i]);
         }
         else{
             I_type(imem[i]);
         }
+        if(error_halt){
+            break;
+        }
+
         if(!pcChanged)
             PC = PC + 4;
-
-        cycle++;
         print_cycle();
         i = (PC-PC_init)/4;
        // printf("i: %d\n",i);
@@ -107,21 +119,54 @@ void print_cycle(void)
         fprintf(snapshot, "$%02d: 0x%08X\n",i,reg[i]);
     fprintf(snapshot,"PC: 0x%08X\n\n\n",PC);
 }
+void print_error(int type, int cyc)
+{
+    switch(type){
+        case WRITE_ZERO:
+            fprintf( error_dump , "In cycle %d: Write $0 Error\n", cyc);
+            break;
+        case NUMBER_OVERFLOW:
+            fprintf(error_dump , "In cycle %d: Number Overflow\n", cyc);
+            break;
+        case ADDRESS_OVERFLOW:
+            fprintf(error_dump , "In cycle %d: Address Overflow\n", cyc);
+            break;
+        case MISALIGNMENT:
+            fprintf(error_dump , "In cycle %d: Misalignment Error\n", cyc);
+            break;
+    }
+}
+bool has_overflow(int c, int a, int b)
+{
+    if( (a>>31)==(b>>31) && (a>>31)!=(c>>31) )
+        return true;
+    return false;
+}
 void R_type(unsigned int instr)
 {
     int funct = ( instr << 26 ) >> 26;
-    unsigned int rs, rt, rd, C;
-    rs = (instr<<6) >>27;
-    rt = (instr<<11)>>27;
-    rd = (instr<<16)>>27;
-    C  = (instr<<21)>>27;
+    unsigned int rs = (instr<<6) >>27;
+    unsigned int rt = (instr<<11)>>27;
+    unsigned int rd = (instr<<16)>>27;
+    unsigned int C  = (instr<<21)>>27;
+
+    if(funct!=JR && rd==0 && instr!=0x00000000){
+        print_error(WRITE_ZERO, cycle);
+        return;
+    }
 
     switch(funct){
     case ADD:
         reg[rd] = reg[rs] + reg[rt];
+        if( has_overflow(reg[rd], reg[rs], reg[rt]) )
+            print_error(NUMBER_OVERFLOW, cycle);
+
         puts("add"); break;
     case SUB:
         reg[rd] = reg[rs] - reg[rt];
+        if( has_overflow(reg[rd], reg[rs], -reg[rt]) )
+            print_error(NUMBER_OVERFLOW, cycle);
+
         puts("sub"); break;
     case AND:
         reg[rd] = reg[rs] & reg[rt];
@@ -154,12 +199,32 @@ void R_type(unsigned int instr)
         PC = reg[rs];
         pcChanged = true;
         puts("jr");
-        printf("rs: %d, reg[rs]:%08x\n", rs, reg[rs]);
         break;
     default:
         puts("decode fail!");
     }
+
     //printf("%d %d %d\n",rs, rt, rd, C);
+}
+void check_errors(int rt, int addr, int reg_s, int sC, int op)
+{
+    if(rt==0)
+        print_error(WRITE_ZERO, cycle);
+    if( has_overflow(addr, reg_s, sC) )
+        print_error(NUMBER_OVERFLOW, cycle);
+    if(addr >= MEM_SIZE){
+        print_error(ADDRESS_OVERFLOW, cycle);
+        error_halt = true;
+    }
+    int unit;
+    if(op==LW || op==SW) unit = 4;
+    else if(op==LH || op==LHU || SH) unit = 2;
+    else unit = 1;
+
+    if(addr%unit!=0){
+        print_error(MISALIGNMENT, cycle);
+        error_halt = true;
+    }
 }
 void I_type(unsigned int instr)
 {
@@ -168,80 +233,83 @@ void I_type(unsigned int instr)
     unsigned int rt = (instr<<11)>>27;
     unsigned int uC  = (instr<<16)>>16;
     int sC = ((int)(instr<<16))>>16;
-    unsigned int addr = reg[rs] + sC;/**handle memory addr overflow*/
     unsigned int masks[4] = {0xffffff00, 0xffff00ff, 0xff00ffff, 0x00ffffff};
     int shift, save, tmp;
     unsigned int tmpu;
-
+    bool overflow = false;
+    unsigned int addr = reg[rs] + sC;/**handle memory addr overflow*/
 
     switch(opcode){
     case ADDI:
+        if(rt==0){
+            print_error(WRITE_ZERO, cycle);
+        }
         reg[rt] = reg[rs] + sC;
+        if( has_overflow(reg[rt], reg[rs], sC) )
+             print_error(NUMBER_OVERFLOW, cycle);
+        if(rt==0) reg[rt] = 0;
+
         puts("addi"); break;
 
     case LW:
-        if( addr%4 != 0 ){
-            /**error handle*/
-            puts("memory aligned error!!");
-        }else{
-            reg[rt] = dmem[addr/4];
-        }
+        check_errors(rt, addr, reg[rs], sC, LW);
+        if(error_halt) return;
+        reg[rt] = dmem[addr/4];
+        if(rt==0) reg[rt]=0;
+
         puts("lw"); break;
     case LH: //signed
-        if( addr%2 != 0 ){
-            /**error handle*/
-            puts("memory aligned error!!");
-        }else{
-            int tmp = dmem[addr/4];
-            reg[rt] = addr%4==0 ? (tmp<<16)>>16 : tmp>>16;
-        }
+        check_errors(rt, addr, reg[rs], sC, LH);
+        if(error_halt) return;
+        tmp = dmem[addr/4];
+        reg[rt] = addr%4==0 ? (tmp<<16)>>16 : tmp>>16;
+        if(rt==0) reg[rt]=0;
+
         puts("lh"); break;
     case LHU:
-        if( addr%2 != 0 ){
-            /**error handle*/
-            puts("memory aligned error!!");
-        }else{
-            unsigned int tmp = dmem[addr/4];
-            reg[rt] = addr%4==0 ? (tmp<<16)>>16 : tmp>>16;
-        }
+        check_errors(rt, addr, reg[rs], sC, LHU);
+        if(error_halt) return;
+        tmpu = dmem[addr/4];
+        reg[rt] = addr%4==0 ? (tmpu<<16)>>16 : tmpu>>16;
+        if(rt==0) reg[rt]=0;
         puts("lhu"); break;
     case LB:
+        check_errors(rt, addr, reg[rs], sC, LB);
         tmp = dmem[addr/4];
         shift = 24 - (addr%4)*8;
         reg[rt] = (tmp << shift) >> 24;
+        if(rt==0) reg[rt]=0;
 
         puts("lb"); break;
     case LBU:
+        check_errors(rt, addr, reg[rs], sC, LBU);
         tmpu = dmem[addr/4];
         shift = 24 - (addr%4)*8;
         reg[rt] = (tmpu << shift) >> 24;
+        if(rt==0) reg[rt]=0;
 
         puts("lbu"); break;
     case SW:
-        if( addr%4 != 0 ){
-            /**error handle*/
-            puts("memory aligned error!!");
-        }else{
-            dmem[addr/4] = reg[rt];
-        }
+        check_errors(-1, addr, reg[rs], sC, SW);//SW不會有write to zero reg的問題所以rt設-1
+        if(error_halt) return;
+        dmem[addr/4] = reg[rt];
 
         puts("sw"); break;
     case SH:
-        if( addr%2 != 0 ){
-            /**error handle*/
-            puts("memory aligned error!!");
+        check_errors(-1, addr, reg[rs], sC, SH);
+        if(error_halt) return;
+        tmpu = dmem[addr/4];
+        save = reg[rt] & 0x0000ffff;
+        if(addr%4==0){
+            dmem[addr/4] = ((tmpu>>16)<<16) + save;
         }else{
-            tmpu = dmem[addr/4];
-            save = reg[rt] & 0x0000ffff;
-            if(addr%4==0){
-                dmem[addr/4] = ((tmpu>>16)<<16) + save;
-            }else{
-                dmem[addr/4] = (tmpu&0x0000ffff) + (save<<16);
-            }
+            dmem[addr/4] = (tmpu&0x0000ffff) + (save<<16);
         }
 
         puts("sh"); break;
     case SB:
+        check_errors(-1, addr, reg[rs], sC, SB);
+        if(error_halt) return;
         tmpu = dmem[addr/4];
         save = reg[rt] & 0x000000ff;
         shift = (addr%4)*8;
@@ -250,23 +318,45 @@ void I_type(unsigned int instr)
         puts("sb"); break;
 
     case LUI:
+        if(rt==0){
+            print_error(WRITE_ZERO, cycle);
+            return;
+        }
         reg[rt] = uC << 16;
         puts("lui"); break;
     case ANDI:
+        if(rt==0){
+            print_error(WRITE_ZERO, cycle);
+            return;
+        }
         reg[rt] = reg[rs] & uC;
         puts("andi"); break;
     case ORI:
+        if(rt==0){
+            print_error(WRITE_ZERO, cycle);
+            return;
+        }
         reg[rt] = reg[rs] | uC;
         puts("ori"); break;
     case NORI:
+        if(rt==0){
+            print_error(WRITE_ZERO, cycle);
+            return;
+        }
         reg[rt] = ~(reg[rs] | uC);
         puts("nori"); break;
     case SLTI:
+        if(rt==0){
+            print_error(WRITE_ZERO, cycle);
+            return;
+        }
         reg[rt] = ( (int)reg[rs] < sC );
         puts("slti"); break;
 
     case BEQ:
         if(reg[rs]==reg[rt]){
+            if( has_overflow( PC+4+sC*4, PC+4, sC*4) )
+                print_error(NUMBER_OVERFLOW, cycle);
             PC = (PC+4) + sC*4;
             pcChanged = true;
         }
@@ -275,7 +365,8 @@ void I_type(unsigned int instr)
         break;
     case BNE:
         if(reg[rs]!=reg[rt]){
-            printf("");
+            if( has_overflow( PC+4+sC*4, PC+4, sC*4) )
+                print_error(NUMBER_OVERFLOW, cycle);
             PC = (PC+4) + sC*4;
             pcChanged = true;
         }
@@ -306,14 +397,16 @@ void init(void)
     for(int i=0; i<32; i++)
         reg[i] = ZERO;
     reg[29] = sp;
+    for(int i= dSize; i<256; i++)
+        dmem[i] = ZERO;
 
     snapshot = fopen("snapshot.rpt", "w");
     error_dump = fopen("error_dump.rpt", "w");
 }
 bool load_image(void)
 {
-    FILE *fi = fopen("open_testcase\\recur\\iimage.bin", "rb");
-    FILE *fd = fopen("open_testcase\\recur\\dimage.bin", "rb");
+    FILE *fi = fopen("open_testcase\\error2\\iimage.bin", "rb");
+    FILE *fd = fopen("open_testcase\\error2\\dimage.bin", "rb");
     if(!fi || !fd) return false;
 
     unsigned char bytes[4];
@@ -333,6 +426,7 @@ bool load_image(void)
         i++;
     }
     puts("");
+
     i = 0;
     while( fread(bytes, 4, 1, fd) != 0 ){
         if(i==0){
--- test.cpp
+++ test.cpp
@@ -8,11 +8,24 @@ int main()
     unsigned int result = (int(n<<16))>>16;
     unsigned int tmp = 0xffff0000;
     result = (tmp)>>16;
-    printf("%08x\n",result);
+    int a1 = -123456;
+    int a2 = 2147483647;
+    int b = a1-a2;
+    printf("%08x\n",-a2>>31);
+    if( (a1>>31)==( -a2>>31) && (a1>>31)!=(b>>31) )
+        puts("number overflow");
+    else
+        puts("not overflow");
+    unsigned int num = 5;
+    int m = -num;
+    printf("%d\n",m);
+
+
+   /* printf("%08x\n",result);
 
 
     printf("%d\n",n>1);
-    printf("%d\n",(int)n > 1);
+    printf("%d\n",(int)n > 1);*/
 
 
     return 0;
